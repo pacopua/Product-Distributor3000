6fea794a56c3da4f34aa6445996beaa6afa61a58
angel.cantos <angel.cantos@estudiantat.upc.edu>
2024-11-10 21:36:36 +0100

    test division?
    Signed-off-by: angel.cantos <angel.cantos@estudiantat.upc.edu>

---
+++ src/test/java/edu.upc.prop.clusterxx/TestDivisionShould.java
@@ -0,0 +1,23 @@
+package edu.upc.prop.clusterxx;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import edu.upc.prop.clusterxx.Main;
+
+public class TestDivisionShould {
+  @Test
+  public void divideGettingCorrectResult() {
+    Main divisionMachine = new Main();
+
+    float actualResult = divisionMachine.division(10,2);
+    assertEquals(5.0, actualResult, 0.0001);
+  }
+
+  @Test(expected = ArithmeticException.class)
+  public void divideByZeroThrowsException() {
+    Main divisionMachine = new Main();
+
+    float actualResult = divisionMachine.division(10,0);
+
+  }
+}
