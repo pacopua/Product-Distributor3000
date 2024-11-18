04c0246d5d31d86a195b72fb0a5c11e663414335
Adria Cebrian Ruiz <adrisworking@gmail.com>
2024-11-18 15:59:10 +0100

    "tests de algoritmo rapido ya funciona"

--- src/test/java/edu.upc.prop.clusterxx/AlgoritmoRapidoTest.java
+++ src/test/java/edu.upc.prop.clusterxx/AlgoritmoRapidoTest.java
@@ -37,9 +37,7 @@ public class AlgoritmoRapidoTest {
 
         ar = new AlgoritmoRapido(ma4x4);
         s = ar.ejecutar(s, 30);
-        System.out.println("Empece");
         assertTrue(s.getCalidad() >= 80);
-        System.out.println("Acabe");
     }
 
     @Test
