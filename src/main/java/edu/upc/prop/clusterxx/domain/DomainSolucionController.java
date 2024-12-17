d9357061b5e6e4bb68b6cdd7380c3169b4c0757c
Adria Cebrian Ruiz <adria.cebrian@estudiantat.upc.edu>
2024-12-17 13:39:53 +0100

    "  Refactorización de IntercambioController y DomainSolucionController para que se relacionen de manera que sigan la estructura de tres capas "

--- src/main/java/edu/upc/prop/clusterxx/domain/DomainSolucionController.java
+++ src/main/java/edu/upc/prop/clusterxx/domain/DomainSolucionController.java
@@ -2,6 +2,8 @@ package edu.upc.prop.clusterxx.domain;
 
 import edu.upc.prop.clusterxx.data.Sistema;
 
+import java.util.List;
+
 public class DomainSolucionController {
     public void calcularDistribucionRapida(int filas, int columnas) {
         Solucion s = Sistema.nuevaSolucion(filas, columnas);
@@ -14,5 +16,46 @@ public class DomainSolucionController {
         Solucion s = Sistema.nuevaSolucion(filas, columnas);
         AlgoritmoOptimo algo = new AlgoritmoOptimo(Sistema.getMatrizAdyacencia());
         Sistema.setSolucion(algo.ejecutar(s));
+    }
+
+    public List<String> getListaNombresProductos() {
+        return Sistema.getSolucion().getListaProductos().getListaProductos().stream()
+                .map(Producto::getNombre)
+                .toList();
+    }
+
+    public void intercambiarProductos(String nombreP1, String nombreP2) {
+        Solucion solucion = Sistema.getSolucion();
+        Producto p1 = null;
+        for (Producto p : Sistema.getSolucion().getListaProductos().getListaProductos()) {
+            if (nombreP1.equals(p.getNombre())) p1 = p;
+        }
+        //Do the same for p2
+        Producto p2 = null;
+        for (Producto p : Sistema.getSolucion().getListaProductos().getListaProductos()) {
+            if (nombreP2.equals(p.getNombre())) p2 = p;
+        }
+
+        Producto[][] distribucionProductos = solucion.getDistribucionProductos();
+        int[][] distribucion = solucion.getDistribucion();
+        int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+        for (int i = 0; i < distribucionProductos.length; i++) {
+            for (int j = 0; j < distribucionProductos[0].length; j++) {
+                if (p1 == distribucionProductos[i][j]) {
+                    x1 = i;
+                    y1 = j;
+                }
+                if (p2 == distribucionProductos[i][j]) {
+                    x2 = i;
+                    y2 = j;
+                }
+            }
+        }
+
+        distribucionProductos[x1][y1] = p2;
+        distribucionProductos[x2][y2] = p1;
+        distribucion[x1][y1] = solucion.getListaProductos().getListaProductos().indexOf(p2);
+        distribucion[x2][y2] = solucion.getListaProductos().getListaProductos().indexOf(p1);
     }
 }
--- src/main/java/edu/upc/prop/clusterxx/visual/IntercambioController.java
+++ src/main/java/edu/upc/prop/clusterxx/visual/IntercambioController.java
@@ -1,6 +1,8 @@
 package edu.upc.prop.clusterxx.visual;
 
-import edu.upc.prop.clusterxx.data.Sistema;
+//import edu.upc.prop.clusterxx.data.Sistema;
+//import edu.upc.prop.clusterxx.domain.Producto;
+import edu.upc.prop.clusterxx.domain.DomainSolucionController;
 import edu.upc.prop.clusterxx.domain.Producto;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -8,6 +10,28 @@ import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
+public class IntercambioController {
+    @FXML
+    ChoiceBox<String> producto1;
+    @FXML
+    ChoiceBox<String> producto2;
+    private final DomainSolucionController solucionController = new DomainSolucionController();
+
+    @FXML
+    protected void initialize() {
+        producto1.setItems(FXCollections.observableArrayList(solucionController.getListaNombresProductos()));
+        producto2.setItems(FXCollections.observableArrayList(solucionController.getListaNombresProductos()));
+    }
+
+    @FXML
+    protected void onIntercambiarProducto() {
+        String nombreP1 = producto1.getValue();
+        String nombreP2 = producto2.getValue();
+        solucionController.intercambiarProductos(nombreP1, nombreP2);
+        ((Stage) producto1.getScene().getWindow()).close();
+    }
+}
+/*
 public class IntercambioController {
     @FXML
     ChoiceBox producto1;
@@ -63,3 +87,5 @@ public class IntercambioController {
         ((Stage) producto1.getScene().getWindow()).close();
     }
 }
+
+ */
